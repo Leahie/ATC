1a996df64b67daab313e8dd52b3c4bef7e5b59d4
Leah Zhang <LeahZhang1595995@gmail.com>
2024-10-15 09:08:46 -0400

    remove books

--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -10,28 +10,28 @@ interface Dic {
 
 export default async function books(){
     const [data, setData] = useState({})
-    useEffect(()=>{
-        async function fetchSlides(){
-            try {
-                const response = await axios.post('/api/slides')
-                setData(response.data.data)
-                return response.data.data
-            } catch (error:any) {
-                console.log(error.message)
-            }
-        }
-        console.log(fetchSlides())
+    // useEffect(()=>{
+    //     async function fetchSlides(){
+    //         try {
+    //             const response = await axios.post('/api/slides')
+    //             setData(response.data.data)
+    //             return response.data.data
+    //         } catch (error:any) {
+    //             console.log(error.message)
+    //         }
+    //     }
+    //     console.log(fetchSlides())
         
-    },[])
+    // },[])
     
     
     return(
         <div className={["Books"].join(" ")}>
-            {
+            {/* {
             Object.keys(data).map((key:string)=>(
                 <Box name={data[key as keyof typeof data]["name"]} link = {data[key as keyof typeof data]["webViewLink"]} image = {data[key as keyof typeof data]["thumbnailLink"]}/>
             ))
-        }
+        } */}
         </div>
     )
 }
